d7dcf0c43d6d3ca7923f1344a7224183fb58d5bd
GStorm888 <denchikevstr@icloud.com>
2024-04-27 21:29:07 +0300

    написал функцию min

---
+++ min.py
@@ -0,0 +1,8 @@
+min_number = 0
+def min_number(first_num, second_num):
+    if first_num > second_num:
+        min_number = first_num
+    elif first_num > second_num:
+        min_number = second_num
+    else:
+        min_number = first_num
