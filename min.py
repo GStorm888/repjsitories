bf04d89e509ce88f201bb3db71163b8f98d55068
GStorm888 <denchikevstr@icloud.com>
2024-04-27 21:42:35 +0300

    исправил баг в функции min

--- min.py
+++ min.py
@@ -1,6 +1,6 @@
 min_number = 0
 def min_number(first_num, second_num):
-    if first_num > second_num:
+    if first_num < second_num:
         min_number = first_num
     elif first_num > second_num:
         min_number = second_num
